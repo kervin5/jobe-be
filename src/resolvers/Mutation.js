5f4fa0560cb0e583af7dd67dea0f8d05c5912ee9
Kervin Vasquez <kervinlvh@gmail.com>
2019-08-12 15:53:31 -0700

    Implement endpoints for Skills, Categories, Locations and Jobs

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,3 +1,40 @@
-const Mutations = {};
+const Mutations = {
+    async createUser(parent, args, ctx, info) {
+        const user = await ctx.db.mutation.createUser({
+            data: {
+                ...args
+            }
+        }, info);
+        return user;
+    },
+
+    async createLocation(parent, args, ctx, info) {
+        const location = await ctx.db.mutation.createLocation({
+            data: {
+                ...args
+                
+            }
+        }, info);
+        return location;
+    },
+
+    async createCategory(parent, args, ctx, info) {
+        const category = await ctx.db.mutation.createCategory({
+            data: {
+                ...args
+            }
+        }, info);
+        return category;
+    },
+
+    async createSkill(parent, args, ctx, info) {
+        const skill = await ctx.db.mutation.createSkill({
+            data: {
+                ...args
+            }
+        }, info);
+        return skill;
+    }
+};
 
 module.exports = Mutations;
--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,3 +1,16 @@
-const Query = {};
+const { forwardTo } = require('prisma-binding');
+
+const Query = {
+    // async locations(parent, args, ctx, info) {
+    //     const locations = await ctx.db.query.locations();
+    //     return locations;
+    // }
+    locations: forwardTo('db'),
+    location: forwardTo('db'),
+    categories: forwardTo('db'),
+    category: forwardTo('db'),
+    skills: forwardTo('db'),
+    skill: forwardTo('db')
+};
 
 module.exports = Query;
