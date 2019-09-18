13e38908d06c3035a2ed2d027b9ea0c1fef182ff
Kervin Vasquez <kervinlvh@gmail.com>
2019-09-18 15:21:22 -0700

    Implement mutation to reset password

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -380,6 +380,38 @@ const Mutations = {
          });
 
          return args.email;
+    },
+    async resetPassword(parent, args, ctx, info) {
+         // 1. Check if the passwords match
+         if(args.password !== args.confirmPassword) {
+            throw new Error('Passwords don\'t match!');
+        }
+        // 2. Check if its a legit reset token
+        // 3. Check if its expired
+        const [user] = await ctx.db.query.users({where: {
+            resetToken: args.resetToken,
+            resetTokenExpiry_gte: Date.now() - 3600000
+        }})
+
+        if(!user) throw new Error('This token is either invalid or expired!');
+        // 4. Hash their new password
+        const password = await bcrypt.hash(args.password, 10);
+        // 5. Save the new password to the user and remove old reset token fields
+        const updatedUser = await ctx.db.mutation.updateUser({
+            where: {email: user.email},
+            data: {
+                password,
+                resetToken: null,
+                resetTokenExpiry: null
+            }
+        })
+        // 6. Generate JWT
+        const token = jwt.sign({ id: updatedUser.id }, process.env.APP_SECRET);
+        // 7. Set the JWT cookie
+        ctx.response.cookie('token', token, { httpOnly: true, maxAge: 1000 * 60 * 60 * 24 * 365 } );
+        // 8. Return the new user
+        return updatedUser;
+        // 9. Amazing
     }
 };
 
