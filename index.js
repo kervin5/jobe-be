86933eba8ff33872126fc74570dc62da8c6b3db6
Exact Staff Git <admin@exactstaff.com>
2019-11-05 15:41:40 -0800

    Add express middleware sentry

--- index.js
+++ index.js
@@ -3,11 +3,17 @@ const createServer = require("./startup/createServer");
 const db = require("./startup/db");
 const auth = require("./src/middleware/auth/auth");
 const cookieParser = require("cookie-parser");
+const Sentry = require("@sentry/node");
+Sentry.init({ dsn: process.env.NODE_ENV });
 
 const server = createServer();
 
+// The request handler must be the first middleware on the app
+server.express.use(Sentry.Handlers.requestHandler());
+
 server.express.use(cookieParser());
 server.express.use(auth);
+server.express.use(Sentry.Handlers.errorHandler());
 
 server.start(
   {
