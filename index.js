c0a06f74a05a6f74719c30f147164cb8b3d0f30f
Exact Staff Git <admin@exactstaff.com>
2019-11-05 16:02:36 -0800

    Disable sentry

--- index.js
+++ index.js
@@ -3,17 +3,17 @@ const createServer = require("./startup/createServer");
 const db = require("./startup/db");
 const auth = require("./src/middleware/auth/auth");
 const cookieParser = require("cookie-parser");
-const Sentry = require("@sentry/node");
-Sentry.init({ dsn: process.env.SENTRY_DSN });
+// const Sentry = require("@sentry/node");
+// Sentry.init({ dsn: process.env.SENTRY_DSN });
 
 const server = createServer();
 
 // The request handler must be the first middleware on the app
-server.express.use(Sentry.Handlers.requestHandler());
+// server.express.use(Sentry.Handlers.requestHandler());
 
 server.express.use(cookieParser());
 server.express.use(auth);
-server.express.use(Sentry.Handlers.errorHandler());
+// server.express.use(Sentry.Handlers.errorHandler());
 
 server.start(
   {
--- startup/createServer.js
+++ startup/createServer.js
@@ -3,29 +3,29 @@ const Mutation = require("../src/resolvers/Mutation");
 const Query = require("../src/resolvers/Query");
 const db = require("./db");
 const permissions = require("../src/middleware/auth/permissions");
-const { sentry } = require("graphql-middleware-sentry");
+// const { sentry } = require("graphql-middleware-sentry");
 
-const sentryMiddleware = sentry({
-  config: {
-    dsn: process.env.SENTRY_DSN,
-    environment: process.env.NODE_ENV
-  },
-  withScope: (scope, error, context) => {
-    scope.setUser({
-      id: context.request.user.id
-    });
-    scope.setExtra("body", context.request.body);
-    scope.setExtra("origin", context.request.headers.origin);
-    scope.setExtra("user-agent", context.request.headers["user-agent"]);
-  }
-});
+// const sentryMiddleware = sentry({
+//   config: {
+//     dsn: process.env.SENTRY_DSN,
+//     environment: process.env.NODE_ENV
+//   },
+//   withScope: (scope, error, context) => {
+//     scope.setUser({
+//       user: context.request.user || undefined
+//     });
+//     scope.setExtra("body", context.request.body);
+//     scope.setExtra("origin", context.request.headers.origin);
+//     scope.setExtra("user-agent", context.request.headers["user-agent"]);
+//   }
+// });
 
 // Create the GraphQL Yoga Server
 
 function createServer() {
   return new GraphQLServer({
     typeDefs: "src/schema.graphql",
-    middlewares: [sentryMiddleware, permissions],
+    middlewares: [permissions],
     resolvers: {
       Mutation,
       Query
