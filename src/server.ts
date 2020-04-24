cd103fae4346988651a792b096b38278fd052bc2
Kervin <kervinlvh@gmail.com>
2020-04-23 21:25:21 -0700

    Create middleware for auth

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,30 @@
 import { ApolloServer } from 'apollo-server'
 import { applyMiddleware } from 'graphql-middleware'
 import { schema } from './schema'
-import { createContext } from './context'
-import {permissions} from "./permissions"
+import { createContext, Context } from './context'
+import { permissions } from './permissions'
 
-const schemaWithMiddleware = applyMiddleware(
-  schema, permissions
-)
+const injectUser = async (resolve, root, args, context, info) => {
+  const Authorization = context.headers['authorization']
+  // if (Authorization) {
+  //   const token = Authorization.replace('Bearer ', '')
+  //   const verifiedToken = verify(token, APP_SECRET) as Token
+  //   return verifiedToken && verifiedToken.userId
+  // }
 
-new ApolloServer({ schema: schemaWithMiddleware, context: createContext }).listen(
-  { port: 4000 },
-  () =>
-    console.log(
-      `🚀 Server ready at: http://localhost:4000\n⭐️ See sample queries: http://pris.ly/e/ts/graphql-apollo-server#using-the-graphql-api`,
-    ),
+  console.log(Object.keys(context.headers), Authorization)
+  const result = await resolve(root, args, context, info)
+  console.log(`5. logInput`)
+  return result
+}
+
+const schemaWithMiddleware = applyMiddleware(schema, injectUser, permissions)
+
+new ApolloServer({
+  schema: schemaWithMiddleware,
+  context: createContext,
+}).listen({ port: 4000 }, () =>
+  console.log(
+    `🚀 Server ready at: http://localhost:4000\n⭐️ See sample queries: http://pris.ly/e/ts/graphql-apollo-server#using-the-graphql-api`,
+  ),
 )
