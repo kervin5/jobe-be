3a3dd698e565ef7d5d0d454152bd1c2379125c41
Kervin <kervinlvh@gmail.com>
2020-04-23 21:03:36 -0700

    Assign default value to job search

--- src/resolvers/queries/jobs.ts
+++ src/resolvers/queries/jobs.ts
@@ -1,7 +1,7 @@
 import { searchBoundary } from '../../utils/location'
 //const { forwardTo } = require("prisma-binding");
 import { ObjectDefinitionBlock } from '@nexus/schema/dist/definitions/objectType'
-import { stringArg, arg, intArg, floatArg } from '@nexus/schema'
+import { stringArg, arg, intArg } from '@nexus/schema'
 import { can } from '../../permissions/auth'
 
 interface JobFilter {
@@ -64,7 +64,7 @@ export default (t: ObjectDefinitionBlock<'Query'>) => {
   t.list.field('searchJobs', {
     type: 'Job',
     args: {
-      radius: intArg({ nullable: true }),
+      radius: intArg({ nullable: true, default: 5 }),
       location: stringArg({ nullable: true }),
       query: stringArg(),
       where: arg({ type: 'JobWhereInput' }),
@@ -104,7 +104,7 @@ export default (t: ObjectDefinitionBlock<'Query'>) => {
       const [leftEdge, bottomEdge, rightEdge, topEdge] = await searchBoundary(
         args.location,
         ctx,
-        args.radius,
+        args.radius ?? 5,
       )
 
       return await ctx.prisma.job.findMany({
@@ -183,175 +183,6 @@ export default (t: ObjectDefinitionBlock<'Query'>) => {
     },
   })
 }
-
-/*
-const job = forwardTo("db");
-
-
-
-const jobs = async (parent, args, ctx, info) => {
-  //Default values
-  const status = "POSTED";
-  return await ctx.db.query.jobs(
-    { ...args, where: { ...args.where, status } },
-    info
-  );
-};
-const protectedJobs = async (parent, args, ctx, info) => {
-  if (!ctx.request.user) {
-    return [];
-  }
-
-  const user = await ctx.db.query.user(
-    { where: { id: ctx.request.user.id } },
-    `{id branch { id company { id } } }`
-  );
-
-  //Gets jobs created by this user by default;
-  let ownerFilter = { author: { id: ctx.request.user.id } };
-
-  //Define jobs filter based on access level
-  if (await can("READ", "COMPANY", ctx)) {
-    //Gets all the jobs from the company
-    ownerFilter = { branch: { company: { id: user.branch.company.id } } };
-  } else if (await can("READ", "BRANCH", ctx)) {
-    //Gets all the jobs from the branch
-    ownerFilter = { branch: { id: user.branch.id } };
-  }
-
-  return await ctx.db.query.jobs(
-    { ...args, where: { ...args.where, ...ownerFilter } },
-    info
-  );
-};
-const searchJobs = async (parent, args, ctx, info) => {
-  if (!args.location || args.location === "") {
-    return await ctx.db.query.jobs(
-      {
-        where: {
-          OR: [
-            { title_contains: args.query.toLowerCase() },
-            { description_contains: args.query.toLowerCase() },
-            { title_contains: args.query.toUpperCase() },
-            { description_contains: args.query.toUpperCase() },
-            { title_contains: titleCase(args.query) },
-            { description_contains: titleCase(args.query) },
-            {
-              location: {
-                OR: [
-                  { name_contains: titleCase(args.query) },
-                  { name_contains: args.query.toLowerCase() },
-                  { name_contains: args.query.toUpperCase() }
-                ]
-              }
-            }
-          ],
-          ...args.where,
-          status: "POSTED"
-        },
-        ...(args.perPage ? { perPage: args.perPage, skip: args.skip } : {}),
-        orderBy: "updatedAt_DESC"
-      },
-      info
-    );
-  }
-
-  const [leftEdge, bottomEdge, rightEdge, topEdge] = await searchBoundary(
-    args.location,
-    ctx,
-    args.radius
-  );
-
-  return await ctx.db.query.jobs(
-    {
-      where: {
-        AND: [
-          {
-            OR: [
-              { title_contains: args.query.toLowerCase() },
-              { description_contains: args.query.toLowerCase() },
-              { title_contains: args.query.toUpperCase() },
-              { description_contains: args.query.toUpperCase() },
-              { title_contains: titleCase(args.query) },
-              { description_contains: titleCase(args.query) }
-            ]
-          },
-          {
-            OR: [
-              {
-                location: {
-                  longitude_lte: rightEdge,
-                  longitude_gte: leftEdge,
-                  latitude_lte: topEdge,
-                  latitude_gte: bottomEdge
-                }
-              },
-              {
-                location: {
-                  name_contains: args.location
-                }
-              }
-            ]
-          }
-        ],
-        ...args.where,
-        status: "POSTED"
-      },
-      ...(args.perPage ? { perPage: args.perPage, skip: args.skip } : {}),
-      orderBy: "createdAt_DESC"
-    },
-    info
-  );
-};
-
-const protectedJobsConnection = async (parent, args, ctx, info) => {
-  if (!ctx.request.user) {
-    return null;
-  }
-
-  const user = await ctx.db.query.user(
-    { where: { id: ctx.request.user.id } },
-    `{id branch { id company { id } } }`
-  );
-
-  //Gets jobs created by this user by default;
-  let ownerFilter = { author: { id: ctx.request.user.id } };
-
-  //Define jobs filter based on access level
-  if (await can("READ", "COMPANY", ctx)) {
-    //Gets all the jobs from the company
-    ownerFilter = { branch: { company: { id: user.branch.company.id } } };
-  } else if (await can("READ", "BRANCH", ctx)) {
-    //Gets all the jobs from the branch
-    ownerFilter = { branch: { id: user.branch.id } };
-  }
-  return await ctx.db.query.jobsConnection(
-    { ...args, where: { ...args.where, ...ownerFilter } },
-    info
-  );
-};
-
-const jobsConnection = (parent, args, ctx, info) => {
-  return ctx.db.query.jobsConnection(
-    { ...args, where: { ...args.where, status: "POSTED" } },
-    info
-  );
-};
-
-
-
-module.exports = {
-  queries: {
-    job,
-    jobs,
-    protectedJobs,
-    searchJobs,
-    jobsConnection,
-    protectedJobsConnection
-  }
-};
-
-*/
 
 function titleCase(str: string = '') {
   let string = str.toLowerCase().split(' ')
