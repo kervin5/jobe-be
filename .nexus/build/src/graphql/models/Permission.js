444ce32922d6533435dc96d7215eb856f7ae2f84
Kervin Vasquez <kervinlvh@gmail.com>
2020-06-10 01:25:49 -0700

    node engine update

--- .nexus/build/src/graphql/models/Permission.js
+++
@@ -1,9 +0,0 @@
-import { schema } from 'nexus';
-schema.objectType({
-    name: 'Permission',
-    definition(t) {
-        t.model.id();
-        t.model.object();
-        t.model.actions();
-    },
-});
--- .nexus/build/src/graphql/models/Term.js
+++
@@ -1,9 +0,0 @@
-import { schema } from 'nexus';
-schema.objectType({
-    name: 'Term',
-    definition(t) {
-        t.string('id');
-        t.string('label');
-        t.string('type');
-    },
-});
