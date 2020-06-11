b4137b117f6521fc28c9a76d863882721f06d2ad
Kervin Vasquez <kervinlvh@gmail.com>
2020-06-10 18:51:43 -0700

    end of day

--- .tmp_build/src/graphql/models/AuthPayload.js
+++
@@ -1,8 +0,0 @@
-import { schema } from 'nexus';
-schema.objectType({
-    name: 'AuthPayload',
-    definition(t) {
-        t.string('token');
-        t.field('user', { type: 'User' });
-    },
-});
--- .tmp_build/src/graphql/models/Favorite.js
+++
@@ -1,9 +0,0 @@
-import { schema } from 'nexus';
-schema.objectType({
-    name: 'Favorite',
-    definition(t) {
-        t.model.id();
-        t.model.user();
-        t.model.job();
-    },
-});
