27f48b0aafb62b91036278b5b2389b98ec4b6015
Kervin Vasquez <kvasquez@exactstaff.com>
2020-07-22 16:54:34 -0700

    protect companies endpoint

--- src/permissions/index.ts
+++ src/permissions/index.ts
@@ -17,6 +17,11 @@ const rules = {
 export const permissions = shield({
   rules: {
     Query: {
+      companies: and(
+        rules.isAuthenticatedUser,
+        rules.can({ action: 'CREATE', object: 'JOB' }),
+        rules.can({ action: 'READ', object: 'APPLICATION' }),
+      ),
       application: and(
         rules.isAuthenticatedUser,
         rules.can({ action: 'CREATE', object: 'JOB' }),
