9cdff9bf3fe86367818d60075a16ec1231601033
Kervin Vasquez <kervinlvh@gmail.com>
2020-10-18 01:09:26 -0700

    handle exception of resume parser

--- src/resolvers/mutations/resumes.ts
+++ src/resolvers/mutations/resumes.ts
@@ -15,54 +15,59 @@ export default (t: core.ObjectDefinitionBlock<'Mutation'>) => {
       title: schema.stringArg({ required: true }),
     },
     resolve: async (parent, args, ctx) => {
-      const resumeUrl = await sign_s3_read(args.path)
-      const resumeJson = await request(process.env.RESUME_PARSER_API, {
-        url: resumeUrl,
-      })
-      const phoneNumber = sanitizePhoneNumber(resumeJson?.parts?.phone)
-      const resumeText = `${resumeJson.parts.summary} ${resumeJson.parts.projects}  ${resumeJson.parts.certification} ${resumeJson.parts.certifications} ${resumeJson.parts.positions} ${resumeJson.parts.objective} ${resumeJson.parts.awards} ${resumeJson.parts.skills} ${resumeJson.parts.experience} ${resumeJson.parts.education}`.toLowerCase()
-      const allSkills = await ctx.db.skill.findMany()
-
-      const resumeSkills = findKeywords(
-        resumeText,
-        allSkills.map((skill) => skill.name),
-      ).filter((skill) => skill !== '')
-
-      const filteredSkills = allSkills.filter((skill) =>
-        resumeSkills.includes(skill.name),
-      )
-
-      const skills = filteredSkills.length
-        ? {
-            skills: {
-              connect: filteredSkills.map((skill) => ({ id: skill.id })),
-            },
-          }
-        : {}
-
-      const result = await ctx.db.resume.create({
-        data: {
-          file: {
-            create: { path: args.path, mimetype: args.type },
-          },
-
-          user: {
-            connect: { id: ctx.request.user.id },
-          },
-
-          title: args.title,
-          ...skills,
-        },
-      })
-
-      if (phoneNumber) {
-        await ctx.db.user.update({
-          where: { id: ctx.request.user.id },
-          data: { phone: phoneNumber },
+      try {
+        const resumeUrl = await sign_s3_read(args.path)
+        const resumeJson = await request(process.env.RESUME_PARSER_API, {
+          url: resumeUrl,
         })
-      }
+        const phoneNumber = sanitizePhoneNumber(resumeJson?.parts?.phone)
+        const resumeText = `${resumeJson.parts.summary} ${resumeJson.parts.projects}  ${resumeJson.parts.certification} ${resumeJson.parts.certifications} ${resumeJson.parts.positions} ${resumeJson.parts.objective} ${resumeJson.parts.awards} ${resumeJson.parts.skills} ${resumeJson.parts.experience} ${resumeJson.parts.education}`.toLowerCase()
+        const allSkills = await ctx.db.skill.findMany()
 
-      return result
+        const resumeSkills = findKeywords(
+          resumeText,
+          allSkills.map((skill) => skill.name),
+        ).filter((skill) => skill !== '')
+
+        const filteredSkills = allSkills.filter((skill) =>
+          resumeSkills.includes(skill.name),
+        )
+
+        const skills = filteredSkills.length
+          ? {
+              skills: {
+                connect: filteredSkills.map((skill) => ({ id: skill.id })),
+              },
+            }
+          : {}
+
+        const result = await ctx.db.resume.create({
+          data: {
+            file: {
+              create: { path: args.path, mimetype: args.type },
+            },
+
+            user: {
+              connect: { id: ctx.request.user.id },
+            },
+
+            title: args.title,
+            ...skills,
+          },
+        })
+
+        if (phoneNumber) {
+          await ctx.db.user.update({
+            where: { id: ctx.request.user.id },
+            data: { phone: phoneNumber },
+          })
+        }
+
+        return result
+      } catch (error) {
+        console.log(error.message)
+        return null
+      }
     },
   })
 }
